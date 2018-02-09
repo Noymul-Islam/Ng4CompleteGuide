8a4324eae167e2c561bc5c8bb58dcfc332ec6780
SELISE\noymul.Islam <noymul.chowdhury@selise.ch>
2018-02-09 21:16:14 +0600

    till 51

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes=[];
+  recipes : Recipe[]=[
+    new Recipe('A Test Recipe','This is a test recipe', "https://images.pexels.com/photos/46170/kitchen-scale-weighing-baking-cooking-46170.jpeg?h=350&auto=compress&cs=tinysrgb")
+    ,new Recipe('A Test Recipe','This is a test recipe', "https://images.pexels.com/photos/46170/kitchen-scale-weighing-baking-cooking-46170.jpeg?h=350&auto=compress&cs=tinysrgb")
+    ,new Recipe('A Test Recipe','This is a test recipe', "https://images.pexels.com/photos/46170/kitchen-scale-weighing-baking-cooking-46170.jpeg?h=350&auto=compress&cs=tinysrgb")
+
+  ];
   constructor() { }
 
   ngOnInit() {
