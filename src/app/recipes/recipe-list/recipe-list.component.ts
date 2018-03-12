d12785cec579bc349c1da1a56b668762b02da20a
SELISE\noymul.Islam <noymul.chowdhury@selise.ch>
2018-03-12 20:31:25 +0600

    event

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Output,EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
+
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,6 +8,7 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes : Recipe[]=[
     new Recipe('A Test Recipe','This is a test recipe', "https://images.pexels.com/photos/46170/kitchen-scale-weighing-baking-cooking-46170.jpeg?h=350&auto=compress&cs=tinysrgb")
     ,new Recipe('A Test Recipe','This is a test recipe', "https://images.pexels.com/photos/46170/kitchen-scale-weighing-baking-cooking-46170.jpeg?h=350&auto=compress&cs=tinysrgb")
@@ -17,5 +19,8 @@ export class RecipeListComponent implements OnInit {
 
   ngOnInit() {
   }
+ recipeSelected(recipe: Recipe){
 
+  this.recipeWasSelected.emit(recipe);
+ }
 }
